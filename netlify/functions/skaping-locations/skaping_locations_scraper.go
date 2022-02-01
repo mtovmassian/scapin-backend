be90a5cfc2a80c7c9593ef5eeb877531f49d3eef
Martin Tovmassian <martin.tovmassian@protonmail.com>
2022-02-01 22:44:32 +0100

    from raw locations to struct skaping locations

--- netlify/functions/skaping-locations/skaping_locations_scraper.go
+++ netlify/functions/skaping-locations/skaping_locations_scraper.go
@@ -13,6 +13,18 @@ type LatLng struct {
 type SkapingRawDataLocation = string
 
 type SkapingLocation struct {
+	url      string
+	position LatLng
+	title    string
+}
+
+func ScrapSkapingLocations(rawHtml *string) []SkapingLocation {
+	skapingLocations := []SkapingLocation{}
+	for _, rawDataLocation := range ScrapSkapingRawDataLocations(rawHtml) {
+		skapingLocations = append(skapingLocations, ToSkapingLocation(&rawDataLocation))
+	}
+
+	return skapingLocations
 }
 
 func ScrapSkapingRawDataLocations(rawHtml *string) []SkapingRawDataLocation {
@@ -23,6 +35,14 @@ func ScrapSkapingRawDataLocations(rawHtml *string) []SkapingRawDataLocation {
 		rawDataLocations = append(rawDataLocations, match[1])
 	}
 	return rawDataLocations
+}
+
+func ToSkapingLocation(rawDataLocation *SkapingRawDataLocation) SkapingLocation {
+	return SkapingLocation{
+		url:      ExractSkapingLocationUrl(rawDataLocation),
+		position: ExtractSkapingLocationLatLng(rawDataLocation),
+		title:    ExtractSkapingLocationTitle(rawDataLocation),
+	}
 }
 
 func ExractSkapingLocationUrl(rawDataLocation *SkapingRawDataLocation) string {
@@ -45,4 +65,14 @@ func ExtractSkapingLocationLatLng(rawDataLocation *SkapingRawDataLocation) LatLn
 	lng, _ := strconv.ParseFloat(matches[0][2], 64)
 
 	return LatLng{lat, lng}
+}
+
+func ExtractSkapingLocationTitle(rawDataLocation *SkapingRawDataLocation) string {
+	pattern := regexp.MustCompile(`title:"(.*)"`)
+	matches := pattern.FindAllStringSubmatch(*rawDataLocation, -1)
+	if len(matches) == 0 {
+		return ""
+	}
+
+	return matches[0][1]
 }
--- netlify/functions/skaping-locations/skaping_locations_scraper_test.go
+++ netlify/functions/skaping-locations/skaping_locations_scraper_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestScrapSkapingRawDataLocations_HtmlWithMatches_ListOfRawDataLocations(t *testing.T) {
+func TestScrapSkapingLocations_NominalCase_ListOfSkapingLocations(t *testing.T) {
 	// GIVEN
 	rawHtml := `<!DOCTYPE html>
 	<html class="no-js">
@@ -51,8 +51,94 @@ func TestScrapSkapingRawDataLocations_HtmlWithMatches_ListOfRawDataLocations(t *
 		</script>
 	</head>
 	</html>`
+	rawHtmlNoMatch := `<!DOCTYPE html>
+	<html class="no-js">
+	<head>
+		<script>
+			console.log('Charly Lima');
+		</script>
+	</head>
+	</html>
+	`
+	// WHEN
+	skapingLocations := ScrapSkapingLocations(&rawHtml)
+	skapingLocationsNoMatch := ScrapSkapingLocations(&rawHtmlNoMatch)
+	// THEN
+	expectedSkapingLocation1 := SkapingLocation{
+		url:      "http://www.skaping.com/pra-loup/molanes",
+		position: LatLng{44.36097000, 6.60417300},
+		title:    " PRA LOUP - CLOS DU SERRE (1820m)",
+	}
+	expectedSkapingLocation2 := SkapingLocation{
+		url:      "http://www.skaping.com/ski-macedonia/bistra-mavrovo",
+		position: LatLng{41.64767700, 20.73542600},
+		title:    "#skimacedonia Bistra-Mavrovo",
+	}
+	expectedSkapingLocations := []SkapingLocation{
+		expectedSkapingLocation1,
+		expectedSkapingLocation2,
+	}
+	assert.Equal(t, expectedSkapingLocations, skapingLocations)
+	assert.Empty(t, skapingLocationsNoMatch)
+}
+
+func TestScrapSkapingRawDataLocations_NominalCase_ListOfRawDataLocations(t *testing.T) {
+	// GIVEN
+	rawHtml := `<!DOCTYPE html>
+	<html class="no-js">
+	<head>
+		<script>
+			windows[1] = new google.maps.InfoWindow({
+				content: "<div style=\"width:650px;max-width:100%\"><div id=\"skaping\" style=\"position:relative;width:100%;padding-top:56.25%;\"><iframe src=\"//www.skaping.com/pra-loup/molanes\" allowfullscreen style=\"position:absolute;top:0;left:0;height:100%;width:100%;border:0\"></iframe></div></div><br /><a target=\"_blank\" href=\"http://www.skaping.com/pra-loup/molanes\">http://www.skaping.com/pra-loup/molanes</a>"
+			});
+			markers[1] = new google.maps.Marker({
+				position: new google.maps.LatLng(44.36097000, 6.60417300),
+				animation: google.maps.Animation.DROP,
+				map: map,
+				title:" PRA LOUP - CLOS DU SERRE (1820m)"
+			});
+			markers[1].addListener('click', function() {
+				if (openedWindow) {
+					openedWindow.close();
+				}
+				windows[1].open(map, markers[1]);
+				openedWindow = windows[1];
+			});
+			bounds.extend(markers[1].getPosition());
+		
+
+			windows[2] = new google.maps.InfoWindow({
+				content: "<div style=\"width:650px;max-width:100%\"><div id=\"skaping\" style=\"position:relative;width:100%;padding-top:56.25%;\"><iframe src=\"//www.skaping.com/ski-macedonia/bistra-mavrovo\" allowfullscreen style=\"position:absolute;top:0;left:0;height:100%;width:100%;border:0\"></iframe></div></div><br /><a target=\"_blank\" href=\"http://www.skaping.com/ski-macedonia/bistra-mavrovo\">http://www.skaping.com/ski-macedonia/bistra-mavrovo</a>"
+			});
+			markers[2] = new google.maps.Marker({
+				position: new google.maps.LatLng(41.64767700, 20.73542600),
+				animation: google.maps.Animation.DROP,
+				map: map,
+				title:"#skimacedonia Bistra-Mavrovo"
+			});
+			markers[2].addListener('click', function() {
+				if (openedWindow) {
+					openedWindow.close();
+				}
+				windows[2].open(map, markers[2]);
+				openedWindow = windows[2];
+			});
+			bounds.extend(markers[2].getPosition());
+		</script>
+	</head>
+	</html>`
+	rawHtmlNoMatch := `<!DOCTYPE html>
+	<html class="no-js">
+	<head>
+		<script>
+			console.log('Charly Lima');
+		</script>
+	</head>
+	</html>
+	`
 	// WHEN
 	rawDataLocations := ScrapSkapingRawDataLocations(&rawHtml)
+	rawDataLocationsNoMatch := ScrapSkapingRawDataLocations(&rawHtmlNoMatch)
 	// THEN
 	expectedRawDataLocation1 := `new google.maps.InfoWindow({
 				content: "<div style=\"width:650px;max-width:100%\"><div id=\"skaping\" style=\"position:relative;width:100%;padding-top:56.25%;\"><iframe src=\"//www.skaping.com/pra-loup/molanes\" allowfullscreen style=\"position:absolute;top:0;left:0;height:100%;width:100%;border:0\"></iframe></div></div><br /><a target=\"_blank\" href=\"http://www.skaping.com/pra-loup/molanes\">http://www.skaping.com/pra-loup/molanes</a>"
@@ -76,27 +162,11 @@ func TestScrapSkapingRawDataLocations_HtmlWithMatches_ListOfRawDataLocations(t *
 		expectedRawDataLocation1,
 		expectedRawDataLocation2,
 	}
-	assert.Equal(t, rawDataLocations, expectedRawDataLocations)
+	assert.Equal(t, expectedRawDataLocations, rawDataLocations)
+	assert.Empty(t, rawDataLocationsNoMatch)
 }
 
-func TestScrapSkapingRawDataLocations_HtmlWithoutMatches_EmptyList(t *testing.T) {
-	// GIVEN
-	rawHtml := `<!DOCTYPE html>
-	<html class="no-js">
-	<head>
-		<script>
-			console.log('Charly Lima');
-		</script>
-	</head>
-	</html>
-	`
-	// WHEN
-	rawDataLocations := ScrapSkapingRawDataLocations(&rawHtml)
-	// THEN
-	assert.Empty(t, rawDataLocations)
-}
-
-func TestExractSkapingLocationUrl_RawDataLocationWithMatch_String(t *testing.T) {
+func TestExractSkapingLocationUrl_NominalCase_String(t *testing.T) {
 	// GIVEN
 	rawDataLocation := `new google.maps.InfoWindow({
 		content: "<div style=\"width:650px;max-width:100%\"><div id=\"skaping\" style=\"position:relative;width:100%;padding-top:56.25%;\"><iframe src=\"//www.skaping.com/banffgondola\" allowfullscreen style=\"position:absolute;top:0;left:0;height:100%;width:100%;border:0\"></iframe></div></div><br /><a target=\"_blank\" href=\"http://www.skaping.com/banffgondola\">http://www.skaping.com/banffgondola</a>"
@@ -107,16 +177,7 @@ func TestExractSkapingLocationUrl_RawDataLocationWithMatch_String(t *testing.T) 
 		 map: map,
 		 title:"Banff Gondola"
 	 });`
-	// WHEN
-	locationUrl := ExractSkapingLocationUrl(&rawDataLocation)
-	// THEN
-	expectedLocationUrl := "http://www.skaping.com/banffgondola"
-	assert.Equal(t, locationUrl, expectedLocationUrl)
-}
-
-func TestExractSkapingLocationUrl_RawDataLocationWithoutMatch_EmptyString(t *testing.T) {
-	// GIVEN
-	rawDataLocation := `new google.maps.InfoWindow({
+	rawDataLocationNoMatch := `new google.maps.InfoWindow({
 		content: "<div style=\"width:650px;max-width:100%\"><div id=\"skaping\" style=\"position:relative;width:100%;padding-top:56.25%;\"><iframe src=\"//www.skaping.com/banffgondola\" allowfullscreen style=\"position:absolute;top:0;left:0;height:100%;width:100%;border:0\"></iframe></div></div><br /><a target=\"_blank\" ferh=\"http://www.skaping.com/banffgondola\">http://www.skaping.com/banffgondola</a>"
 	});
 	 markers[22] = new google.maps.Marker({
@@ -127,12 +188,14 @@ func TestExractSkapingLocationUrl_RawDataLocationWithoutMatch_EmptyString(t *tes
 	 });`
 	// WHEN
 	locationUrl := ExractSkapingLocationUrl(&rawDataLocation)
+	locationUrlNoMatch := ExractSkapingLocationUrl(&rawDataLocationNoMatch)
 	// THEN
 	expectedLocationUrl := "http://www.skaping.com/banffgondola"
-	assert.Empty(t, locationUrl, expectedLocationUrl)
+	assert.Equal(t, expectedLocationUrl, locationUrl)
+	assert.Empty(t, locationUrlNoMatch)
 }
 
-func TestExractSkapingLatLng_RawDataLocationWithMatch_LatLng(t *testing.T) {
+func TestExractSkapingLatLng_NominalCase_LatLng(t *testing.T) {
 	// GIVEN
 	rawDataLocation := `new google.maps.InfoWindow({
 		content: "<div style=\"width:650px;max-width:100%\"><div id=\"skaping\" style=\"position:relative;width:100%;padding-top:56.25%;\"><iframe src=\"//www.skaping.com/banffgondola\" allowfullscreen style=\"position:absolute;top:0;left:0;height:100%;width:100%;border:0\"></iframe></div></div><br /><a target=\"_blank\" href=\"http://www.skaping.com/banffgondola\">http://www.skaping.com/banffgondola</a>"
@@ -156,6 +219,46 @@ func TestExractSkapingLatLng_RawDataLocationWithMatch_LatLng(t *testing.T) {
 	latLng := ExtractSkapingLocationLatLng(&rawDataLocation)
 	latLngInvalidFloats := ExtractSkapingLocationLatLng(&rawDataLocationInvalidFloats)
 	// THEN
-	assert.Equal(t, latLng, LatLng{51.14460700, -115.57476600})
-	assert.Equal(t, latLngInvalidFloats, LatLng{0, 0})
+	assert.Equal(t, LatLng{51.14460700, -115.57476600}, latLng)
+	assert.Equal(t, LatLng{0, 0}, latLngInvalidFloats)
+}
+
+func TestExractSkapingLocationTitle_NominalCase_String(t *testing.T) {
+	// GIVEN
+	rawDataLocation := `new google.maps.InfoWindow({
+		content: "<div style=\"width:650px;max-width:100%\"><div id=\"skaping\" style=\"position:relative;width:100%;padding-top:56.25%;\"><iframe src=\"//www.skaping.com/banffgondola\" allowfullscreen style=\"position:absolute;top:0;left:0;height:100%;width:100%;border:0\"></iframe></div></div><br /><a target=\"_blank\" href=\"http://www.skaping.com/banffgondola\">http://www.skaping.com/banffgondola</a>"
+	});
+	 markers[22] = new google.maps.Marker({
+		 position: new google.maps.LatLng(51.14460700, -115.57476600),
+		 animation: google.maps.Animation.DROP,
+		 map: map,
+		 title:"Banff Gondola"
+	 });`
+	// WHEN
+	locationTitle := ExtractSkapingLocationTitle(&rawDataLocation)
+	// THEN
+	expectedLocationTitle := "Banff Gondola"
+	assert.Equal(t, expectedLocationTitle, locationTitle)
+}
+
+func TestToSkapingLocation_NominalCase_SkapingLocation(t *testing.T) {
+	// GIVEN
+	rawDataLocation := `new google.maps.InfoWindow({
+		content: "<div style=\"width:650px;max-width:100%\"><div id=\"skaping\" style=\"position:relative;width:100%;padding-top:56.25%;\"><iframe src=\"//www.skaping.com/banffgondola\" allowfullscreen style=\"position:absolute;top:0;left:0;height:100%;width:100%;border:0\"></iframe></div></div><br /><a target=\"_blank\" href=\"http://www.skaping.com/banffgondola\">http://www.skaping.com/banffgondola</a>"
+	});
+	 markers[22] = new google.maps.Marker({
+		 position: new google.maps.LatLng(51.14460700, -115.57476600),
+		 animation: google.maps.Animation.DROP,
+		 map: map,
+		 title:"Banff Gondola"
+	 });`
+	// WHEN
+	skapingLocation := ToSkapingLocation(&rawDataLocation)
+	// THEN
+	expectedSkapingLocation := SkapingLocation{
+		url:      "http://www.skaping.com/banffgondola",
+		position: LatLng{51.14460700, -115.57476600},
+		title:    "Banff Gondola",
+	}
+	assert.Equal(t, expectedSkapingLocation, skapingLocation)
 }
